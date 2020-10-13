22c75066ead6601f41f99fb7a53bcd91e5bdd23f
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-13 20:54:10 +0530

    Weather class created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather_bloc/blocs/blocs.dart';
 import 'package:flutter_weather_bloc/repositories/repositories.dart';
 import 'package:flutter_weather_bloc/simple_bloc_observer.dart';
+import 'package:flutter_weather_bloc/widgets/weather.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -14,4 +17,23 @@ void main() {
   );
 
   runApp(App(weatherRepository: weatherRepository));
+}
+
+class App extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+
+  const App({Key key, this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Weather',
+      home: BlocProvider(
+        create: (context) => WeatherBloc(weatherRepository: weatherRepository),
+        child: Weather(),
+      ),
+    );
+  }
 }
