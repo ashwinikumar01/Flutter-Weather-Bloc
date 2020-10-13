cd1225c5602145a0348d5d68eb6090639cad4fe0
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-13 18:22:43 +0530

    Initial commit

---
+++ lib/models/weather.dart
@@ -0,0 +1,108 @@
+import 'package:equatable/equatable.dart';
+
+enum WeatherCondition {
+  // https://www.metaweather.com/api/   (taking icons)
+
+  snow,
+  sleet,
+  hail,
+  thunderstorm,
+  heavyRain,
+  lightRain,
+  showers,
+  heavyCloud,
+  lightCloud,
+  clear,
+  unknown,
+}
+
+class Weather extends Equatable {
+  final WeatherCondition condition;
+  final String formattedCondition; //"weather_state_name": "Heavy Rain",
+  final double minTemp;
+  final double temp;
+  final double maxTemp;
+  final int locationId; //6252230735822848
+  final String created; //"2020-10-13T09:20:02.790665Z"
+  final DateTime lastUpdated; //live time
+  final String location;
+
+  Weather(
+      this.condition,
+      this.formattedCondition,
+      this.minTemp,
+      this.temp,
+      this.maxTemp,
+      this.locationId,
+      this.created,
+      this.lastUpdated,
+      this.location);
+
+  @override
+  List<Object> get props => [
+        condition,
+        formattedCondition,
+        minTemp,
+        temp,
+        maxTemp,
+        locationId,
+        created,
+        lastUpdated,
+        location,
+      ];
+
+  static Weather fromJson(dynamic json) {
+    final consolidatedWeather = json['consolidated_weather'][0];
+    return Weather(
+      condition: _mapStringToWeatherCondition(
+          consolidatedWeather['weather_state_abbr']), //icon
+      formattedCondition: consolidatedWeather['weather_state_name'],
+      minTemp: consolidatedWeather['min_temp'] as double,
+      temp: consolidatedWeather['the_temp'] as double,
+      maxTemp: consolidatedWeather['max_temp'] as double,
+      locationId: json['woeid'] as int,
+      created: consolidatedWeather['created'],
+      lastUpdated: DateTime.now(),
+      location: json['title'],
+    );
+  }
+
+  static WeatherCondition _mapStringToWeatherCondition(String input) {
+    WeatherCondition state;
+    switch (input) {
+      case 'sn':
+        state = WeatherCondition.snow;
+        break;
+      case 'sl':
+        state = WeatherCondition.sleet;
+        break;
+      case 'h':
+        state = WeatherCondition.hail;
+        break;
+      case 't':
+        state = WeatherCondition.thunderstorm;
+        break;
+      case 'hr':
+        state = WeatherCondition.heavyRain;
+        break;
+      case 'lr':
+        state = WeatherCondition.lightRain;
+        break;
+      case 's':
+        state = WeatherCondition.showers;
+        break;
+      case 'hc':
+        state = WeatherCondition.heavyCloud;
+        break;
+      case 'lc':
+        state = WeatherCondition.lightCloud;
+        break;
+      case 'c':
+        state = WeatherCondition.clear;
+        break;
+      default:
+        state = WeatherCondition.unknown;
+    }
+    return state;
+  }
+}
