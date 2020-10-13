47c3d692dddbcf9d31cfea589f00f747d6c9102f
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-13 20:39:08 +0530

    Added Blocobserver in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather_bloc/repositories/repositories.dart';
+import 'package:flutter_weather_bloc/simple_bloc_observer.dart';
+import 'package:http/http.dart' as http;
 
+void main() {
+  Bloc.observer = SimpleBlocObserver();
+
+  final WeatherRepository weatherRepository = WeatherRepository(
+    weatherApiClient: WeatherApiClient(
+      httpClient: http.Client(),
+    ),
+  );
+
+  runApp(App(weatherRepository: weatherRepository));
+}
