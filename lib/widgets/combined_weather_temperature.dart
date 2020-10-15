b5c591e452d12467e918c6816bd6d25491103ee3
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-15 19:09:13 +0530

    Weather App Completed

--- lib/widgets/combined_weather_temperature.dart
+++ lib/widgets/combined_weather_temperature.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather_bloc/blocs/blocs.dart';
 import 'package:flutter_weather_bloc/models/models.dart' as model;
 import './widgets.dart';
 import 'package:meta/meta.dart';
@@ -25,10 +27,15 @@ class CombinedWeatherTemperature extends StatelessWidget {
             ),
             Padding(
               padding: EdgeInsets.all(20.0),
-              child: Temperature(
-                temperature: weather.temp,
-                high: weather.maxTemp,
-                low: weather.minTemp,
+              child: BlocBuilder<SettingBloc, SettingState>(
+                builder: (context, state) {
+                  return Temperature(
+                    temperature: weather.temp,
+                    high: weather.maxTemp,
+                    low: weather.minTemp,
+                    units: state.temperatureUnits,
+                  );
+                },
               ),
             ),
           ],
--- lib/widgets/temperature.dart
+++ lib/widgets/temperature.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_bloc/blocs/blocs.dart';
 
 class Temperature extends StatelessWidget {
   final double temperature;
   final double low;
   final double high;
+  final TemperatureUnits units;
 
   Temperature({
     Key key,
     this.temperature,
     this.low,
     this.high,
+    this.units,
   }) : super(key: key);
 
   @override
@@ -51,5 +54,8 @@ class Temperature extends StatelessWidget {
     );
   }
 
-  int _formattedTemperature(double t) => t.round();
+  int _toFahrenheit(double celsius) => ((celsius * 9 / 5) + 32).round();
+
+  int _formattedTemperature(double t) =>
+      units == TemperatureUnits.fahrenheit ? _toFahrenheit(t) : t.round();
 }
